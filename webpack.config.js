39c996b66dddf2cce8f2a0a3e2d517f1b37189d7
rixlabs <rixlabs@gmail.com>
2017-10-12 23:50:10 +0200

    fixed build and added tslint with airbnb config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-const DIST_PATH = path.resolve( __dirname, './dist' );
-const SOURCE_PATH = path.resolve( __dirname, './app' );
+const DIST_PATH = path.resolve(__dirname, './dist');
+const SOURCE_PATH = path.resolve(__dirname, './app');
 
 module.exports = {
   entry: './app/ts/index.ts',
@@ -14,17 +14,23 @@ module.exports = {
     port: 8888,
   },
   module: {
-    loaders: [{
-      test: /.ts?$/,
-      loader: 'ts-loader'
-    }],
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        { loader: "style-loader" },
-        { loader: "css-loader" },
-        { loader: "sass-loader" }
-      ]
-    }]
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'tslint-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      }]
   }
 };
